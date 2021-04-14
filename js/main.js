cecd409976c1ca7a0bc38cc586cee972f4bdf103
thalesfelippe <72764998+thalesfelippe@users.noreply.github.com>
2021-04-14 14:46:11 -0300

    Terminado a base da pagina com theme dark mode

---
+++ js/main.js
@@ -0,0 +1,148 @@
+// $(window).on("load",function(){
+//     // Preloader
+//     $(".preloader").fadeOut("slow");
+// });
+
+$(document).ready(function(){
+
+    // Navbar shrink
+    $(window).on("scroll",function(){
+        if($(this).scrollTop() > 90){
+            $(".navbar").addClass("navbar-shrink");
+        }
+        else{
+            $(".navbar").removeClass("navbar-shrink");
+        }
+    });
+
+    // video popup
+    const videoSrc = $("#player-1").attr("src");
+    $(".video-play-btn, .video-popup").on("click", function(){
+        if($(".video-popup").hasClass("open")){
+            $(".video-popup").removeClass("open");
+            $("#player-1").attr("src","");
+        }
+        else{
+            $(".video-popup").addClass("open");
+            if($("#player-1").attr("src")==''){
+                $("#player-1").attr("src",videoSrc);
+            }
+        }
+    });
+
+    // features carousel
+    $('.features-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
+
+    // screenshot carousel
+    $('.screenshots-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:1,
+            },
+            1000:{
+                items:1,
+            }
+        }
+    });
+
+    // testimonials carousel
+    $('.testimonials-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
+
+    // Team carousel
+    $('.team-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        autoplay:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
+
+    // Navbar Collapse
+    $(".nav-link").on("click", function(){
+        $(".navbar-collapse").collapse("hide")
+    });
+
+
+    // Toggle Theme - Light & Dark mode
+    function toggleTheme(){
+        if(localStorage.getItem("levelup-theme") !== null){
+            if(localStorage.getItem("levelup-theme") === "dark"){
+                $("body").addClass("dark");
+            }
+            else{
+                $("body").removeClass("dark");
+            }
+        }
+        updateIcon();
+    }
+    toggleTheme();
+    $(".toggle-theme").on("click",function(){
+        $("body").toggleClass("dark");
+        if($("body").hasClass("dark")){
+            localStorage.setItem("levelup-theme","dark");
+        }
+        else{
+            localStorage.setItem("levelup-theme","light");
+        }
+        updateIcon();
+    });
+    function updateIcon(){
+        if($("body").hasClass("dark")){
+            $(".toggle-theme i").removeClass("fa-moon");
+            $(".toggle-theme i").addClass("fa-sun");
+        }
+        else{
+            $(".toggle-theme i").removeClass("fa-sun");
+            $(".toggle-theme i").addClass("fa-moon");
+        }
+    }
+});
